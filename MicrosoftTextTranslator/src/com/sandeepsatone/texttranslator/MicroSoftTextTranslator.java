2931dc2508b8d19fdaf992ef72d960489672f23f
SandyRocks <sandy2711323@gmail.com>
2016-09-25 12:49:23 +0530

    Added OUTPUT

--- MicrosoftTextTranslator/src/com/sandeepsatone/texttranslator/MicroSoftTextTranslator.java
+++ MicrosoftTextTranslator/src/com/sandeepsatone/texttranslator/MicroSoftTextTranslator.java
@@ -6,14 +6,19 @@ import com.memetix.mst.translate.Translate;
 public class MicroSoftTextTranslator {
 	public static void main(String[] args) throws Exception {
 		// Replace client_id and client_secret with your own.
-		Translate.setClientId("<YOUR_CLIENT_ID>");
-		Translate.setClientSecret("YOUR_CLIENT_SECRET");
+		Translate.setClientId("<YOUR_CLIENT_ID");
+		Translate.setClientSecret("<YOUR_APP_SECRET>");
 
 		// Translate an English string to Hindi
 		String englishString = " This Application is developed by SANDEEP ";
 		String hindiTranslation = Translate.execute(englishString, Language.HINDI);
 		System.out.println("Original English : " + englishString);
 		System.out.println("Translated Hindi : " + hindiTranslation);
+/*		
+ *      OUTPUT:
+ *      Original English :  This Application is developed by SANDEEP 
+		Translated Hindi :  संदीप ने इस अनुप्रयोग विकसित की है 
+*/
 
 	}
 
